558d322722556ffc3a831336afca81662fb79acd
juanj <juanj@DESKTOP-M27PG8M>
2019-02-06 13:16:45 +0100

    as

---
+++ src/Endes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Endes {
+
+    public static void main(String[] args) {
+    Scanner S=new Scanner(System.in);
+    int contador=0,n=0,longitud;
+    char letra;
+    String palabra;
+    System.out.println("Introduzca una palabra");
+    palabra=S.nextLine();
+    System.out.println("Introduzca una letra");
+    letra=S.next().charAt(0);
+    longitud=palabra.length();
+    if(longitud>0){
+        do{
+            if(palabra.charAt(n)==letra){
+                contador=contador+1;
+            }
+            n=n+1;
+            longitud=longitud+1;
+        }while(longitud>0);
+    }
+    System.out.println("El numero de veces que aparece la letra en la palabra es" + contador);
+    }
+    
+}
